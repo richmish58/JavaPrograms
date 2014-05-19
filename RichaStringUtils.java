9a76b1d1990bdc4872bc56f8ac3314b3460abc15
richmish58 <richmish58@gmail.com>
2014-05-20 04:25:51 +0530

    Update RichaStringUtils.java

--- RichaStringUtils.java
+++ RichaStringUtils.java
@@ -15,4 +15,52 @@ public class RichaStringUtils extends StringUtilsBase {
     return true
   }
   
+  /**
+  * <p>Reverse a String.</p>
+  *
+  * <pre>
+  *
+  * </pre>
+  *
+  * @param str  the String to reverse, may be blank
+  * @return 
+  */
+  public static String strRev(String str) {
+    
+    return "";
+  }
+  
+  /**
+  * <p>Reverse a String with recursive algorithm.</p>
+  *
+  * <pre>
+  *
+  * </pre>
+  *
+  * @param str  a String to reverse, may be blank
+  * @return 
+  */
+  public static String strRevRec(String str) {
+    
+    return "";
+  }
+
+  /**
+  * <p>Count the occurance of each character in a String.</p>
+  *
+  * <pre>
+  *
+  * </pre>
+  *
+  * @param str  a String in which character occurance to be counted
+  * @return m  a HashMap with each present character as key and number of occurance of those character as value
+  */  
+  public Map<Character, Integer> findCharacterOccurance(String str) {
+      Map<Character, Integer> map = new HashMap<Character, Integer>();
+      
+      return m;
+  }
+  
+  
+  
 }
