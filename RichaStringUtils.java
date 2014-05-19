80a3b4930d9a1eb33ab7794e9df35844a6b669a2
richmish58 <richmish58@gmail.com>
2014-05-20 04:05:42 +0530

    Create RichaStringUtils.java

---
+++ RichaStringUtils.java
@@ -0,0 +1,18 @@
+public class RichaStringUtils extends StringUtilsBase {
+    
+  /**
+  * <p>Checks if a String is palindrome.</p>
+  *
+  * <pre>
+  *
+  * </pre>
+  *
+  * @param str  the String to check, may be blank
+  * @return <code>true</code> if the String is not blank and is plaindrome
+  */
+  public static boolean isPalindrome(String str) {
+    
+    return true
+  }
+  
+}
